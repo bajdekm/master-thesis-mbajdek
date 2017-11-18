8162b7e38b8e4a5eda34cb3caf01a85897f19c02
mbajdek <mbajdek@outlook.com>
2017-11-18 17:48:47 +0100

    chantaje

--- src/main/java/pl/lodz/p/mgr/controllers/RestController.java
+++ src/main/java/pl/lodz/p/mgr/controllers/RestController.java
@@ -5,12 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.lodz.p.mgr.models.BookCategory;
+import pl.lodz.p.mgr.repo.BookCategoryRepository;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/i")
 public class RestController {
+
+    @Autowired
+    BookCategoryRepository repository;
+
+    @RequestMapping(value="/all",method = RequestMethod.GET)
+    public @ResponseBody List<BookCategory> getBC(){
+        return repository.findAll();
+    }
+
 //
 //    @Autowired
 //    private TestModelService testModelService;
